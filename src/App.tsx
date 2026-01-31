c7031636cca8013e9e41654b60c33161626d5f19
LittleBit <littlebit@littlebitstudios.com>
2026-01-31 17:47:38 -0600

    HTTP/S issue fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ async function createOAuthClient() {
   })
 
   const client = new BrowserOAuthClient({
+    allowHttp: true, // Allow HTTP for localhost development
     clientMetadata: {
       ...clientMetadata,
       client_name: 'SkyChat',
